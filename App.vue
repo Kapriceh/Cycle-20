af674df76b74aff24200242cf3d86a2d6e031962
Kapriceh <khart@icstars.org>
2026-03-06 11:54:24 -0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+
+<div>
+
+  <main>
+    <Home />
+    <Projects />
+    <Aboutsection/>
+
+
+
+</main>
+
+</div>
+
+
+</template>
+
+<script>
+import home from './components/home.vue';
+import projects from './components/projects.vue';
+import Aboutsection from './components/Aboutsection';
+import Home from './components/home.vue';
+import Home from './components/home.vue';
+import Project from './components/project.vue';
+
+</script>
+
+<style>
+.cent{
+  
+}
+margin 0;
+
+</style>
